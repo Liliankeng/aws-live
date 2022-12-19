d2e1d12884b1084cfac3dd9aa9b51c7617de60d3
Liliankeng <86642297+Liliankeng@users.noreply.github.com>
2022-12-19 11:01:53 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "employee.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
+customhost = "employee.cwauc1bffyuc.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee"
+custombucket = "kenglilian-employee"
 customregion = "us-east-1"
